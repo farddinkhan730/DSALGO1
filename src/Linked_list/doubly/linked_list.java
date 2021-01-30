4acb6a0d7a10a474f62b6eb38e3a3dadea4a7e46
farddinkhan730 <farddin.khan_cs19@gla.ac.in>
2021-01-30 23:41:43 +0530

    swap_and_partition

--- src/Linked_list/doubly/linked_list.java
+++ src/Linked_list/doubly/linked_list.java
@@ -1,5 +1,8 @@
 package Linked_list.doubly;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class linked_list {
     Node head;
     class Node{
@@ -84,6 +87,198 @@ public class linked_list {
         while (h!=null){
             System.out.print(h.data+" ");
             h=h.prv;
+        }
+    }
+}
+class Quick_sort_in_Linked_list{
+    Node head;
+    class Node{
+        int data;
+        Node next;
+        Node prev;
+        public Node(int d){
+            data=d;
+            next=null;
+            prev=null;
+        }
+    }
+    public void push(int d){
+        Node new_node=new Node(d);
+        if(head==null){
+            head=new_node;
+        }
+        else {
+            new_node.prev=null;
+            new_node.next=head;
+            if(head.prev==null){
+                head.prev=new_node;
+            }
+            head=new_node;
+        }
+
+    }
+    public Node parition(Node l,Node h){
+        int pivot=h.data;
+        Node i=l.prev;
+        for(Node j=l;j!=h;j=j.next){
+            if(j.data<=pivot){
+                i=(i==null)? l : i.next;
+                int temp=i.data;
+                i.data=j.data;
+                j.data=temp;
+            }
+        }
+        i=(i==null)?l:i.next;
+        int temp1=i.data;
+        i.data=pivot;
+        h.data=temp1;
+        return i;
+    }
+    public void quicksort(Node l,Node h){
+        if(h!=null && l!=h && l!=h.next) {
+            Node part = parition(l,h);
+            quicksort(l,part.prev);
+            quicksort(part.next,h);
+        }
+
+    }
+    public static void main(String[] args) {
+        Quick_sort_in_Linked_list ls=new Quick_sort_in_Linked_list();
+        ls.push(45);
+        ls.push(76);
+        ls.push(78);
+        Node nn= ls.head;
+        Node nm=ls.head;
+        while (nm.next!=null){
+            nm=nm.next;
+        }
+        ls.quicksort(ls.head,nm);
+        while (ls.head!=null){
+            System.out.print(ls.head.data+" ->");
+            nn=ls.head;
+            ls.head=ls.head.next;
+        }
+        System.out.println();
+        System.out.println("Reverse of sorted linked list");
+        while (nn!=null){
+            System.out.print(nn.data+" ->");
+            nn=nn.prev;
+        }
+    }
+}
+// in singlly linked list
+class swap_kth_node_first_and_last{
+    Node head;
+    class Node{
+        Node next;
+        int data;
+        Node(int d){
+            data=d;
+            next=null;
+        }
+    }
+    public void push(int x){
+        Node new_node=new Node(x);
+        new_node.next=head;
+        head=new_node;
+//        Node n=head;
+//        if(head==null)
+//        {
+//            head=new_node;
+//            return;
+//        }
+//        while (n.next!=null){
+//            n=n.next;
+//        }
+//        n.next=new_node;
+    }
+    int countNodes()
+    {
+        int count = 0;
+        Node s = head;
+        while (s != null) {
+            count++;
+            s = s.next;
+        }
+        return count;
+    }
+    public void swap(int k){
+//        Node h=head;
+//        int count=1;
+//        while(h.next!=null && count!=k-1) {
+//            count++;
+//            h = h.next;
+//        }
+//        Node hn=head;
+//        int count1=1;
+//        while (hn.next!=null){
+//            count1++;
+//            hn=hn.next;
+//        }
+//        System.out.println(count1+" c " +count);
+//        int d=count1-(count+1);
+//        System.out.println(d);
+//        Node temp=head;
+//        while(temp!=null && d!=(count1-(count+1))){
+//            temp=temp.next;
+//        }
+//        Node temp1=h.next;
+//        Node temp2=temp.next;
+//        h.next=temp.next;
+//        h.next.next=temp1.next;
+//        temp.next=temp1;
+//        temp1.next=temp2.next;
+////        temp2.next=temp.next.next;
+        int n = countNodes();
+
+        if (n < k)
+            return;
+
+        if (2 * k - 1 == n)
+            return;
+
+        Node x = head;
+        Node x_prev = null;
+        for (int i = 1; i < k; i++) {
+            x_prev = x;
+            x = x.next;
+        }
+
+        Node y = head;
+        Node y_prev = null;
+        for (int i = 1; i < n - k + 1; i++) {
+            y_prev = y;
+            y = y.next;
+        }
+
+        if (x_prev != null)
+            x_prev.next = y;
+
+        if (y_prev != null)
+            y_prev.next = x;
+
+        Node temp = x.next;
+        x.next = y.next;
+        y.next = temp;
+
+//        if (k == 1)
+//            head = y;
+//
+//        if (k == n)
+//            head = x;
+    }
+    public static void main(String[] args) {
+        swap_kth_node_first_and_last ls=new swap_kth_node_first_and_last();
+        ls.push(5);
+        ls.push(4);
+        ls.push(3);
+        ls.push(2);
+        ls.push(1);
+        ls.swap(2);
+        Node f=ls.head;
+        while(f!=null){
+            System.out.print(f.data+" ->");
+            f=f.next;
         }
     }
 }
