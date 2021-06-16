463378379f216d62f46eaa3298f8dfd6b2c3c063
farddinkhan730 <farddin.khan_cs19@gla.ac.in>
2021-06-17 00:48:55 +0530

    cp

--- src/LeetCode/Stack/Stack/valid_Parantheses.java
+++ src/LeetCode/Stack/Stack/valid_Parantheses.java
@@ -6,40 +6,58 @@ import java.util.Stack;
 public class valid_Parantheses {
     public static boolean valid(String s){
         Stack<Character> st=new Stack();
-        for (int i = 0; i < s.length(); i++) {
-            if(s.charAt(i)=='('){
-                st.push('(');
-            }
-            else if(s.charAt(i)=='{'){
-                st.push('{');
-            }
-            else if(s.charAt(i)=='[')
-                st.push('[');
-
-            if(s.charAt(i)==')' ) {
-                if(st.isEmpty())
-                    return false;
-                if(st.peek()=='(' ){
-                    st.pop();
-                }
-                else break;
-            }
-            if (s.charAt(i)=='}'){
-                if(st.isEmpty())
-                    return false;
-                if(st.peek()=='{' )
-                    st.pop();
-                else break;
-            }
-            if (s.charAt(i)==']' )
-                if(st.isEmpty())
-                    return false;
-
-            if (st.peek()== '[' )
-                st.pop();
-            else break;
-
+        for (int i = 0; i < s.length(); i++)
+            switch (s.charAt(i)){
+                case '(': st.push('(');
+                break;
+                case '{': st.push('{');
+                break;
+                case '[': st.push('[');
+                break;
+                case ')': if ( st.size()==0 ||'('!=st.peek() ) return false;
+                            else st.pop();;
+                            break;
+                case '}':if (st.size()==0 || '{'!=st.peek()) return false;
+                            else  st.pop();
+                    break;
+                case ']':if (st.size()==0 || '['!=st.peek()) return false;
+                        else st.pop();
+                    break;
         }
+//        for (int i = 0; i < s.length(); i++) {
+//            if(s.charAt(i)=='('){
+//                st.push('(');
+//            }
+//            else if(s.charAt(i)=='{'){
+//                st.push('{');
+//            }
+//            else if(s.charAt(i)=='[')
+//                st.push('[');
+//
+//            if(s.charAt(i)==')' ) {
+//                if(st.isEmpty())
+//                    return false;
+//                if(st.peek()=='(' ){
+//                    st.pop();
+//                }
+//                else break;
+//            }
+//            if (s.charAt(i)=='}'){
+//                if(st.isEmpty())
+//                    return false;
+//                if(st.peek()=='{' )
+//                    st.pop();
+//                else break;
+//            }
+//            if (s.charAt(i)==']' )
+//                if(st.isEmpty())
+//                    return false;
+//
+//            if (st.peek()== '[' )
+//                st.pop();
+//            else break;
+//
+//        }
         if (st.isEmpty()){
             return true;
         }
@@ -47,7 +65,7 @@ public class valid_Parantheses {
 
     }
     public static void main(String[] args) {
-        String s="{}k";
+        String s="[]";
         System.out.println(valid(s));
     }
 }
